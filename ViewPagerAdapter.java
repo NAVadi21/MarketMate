7b50d7b11c73091d5a99e7d590fbb7ad4c7f2a53
Pranav Aditya Singh <93078167+NAVadi21@users.noreply.github.com>
2023-06-11 21:34:22 +0530

    ViewPagerAdapter to ViewPagerAdapter.java

--- ViewPagerAdapter.java
+++ ViewPagerAdapter.java
@@ -1,6 +1,6 @@
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentPagerAdapter; 
 
 public class ViewPagerAdapter extends FragmentPagerAdapter {
 
