72fcb109d4bbcd4477a185aef103d305584d6f9a
Pranav Aditya Singh <93078167+NAVadi21@users.noreply.github.com>
2023-06-21 01:05:00 +0530

    Update ShopAdapter.java

--- ShopAdapter.java
+++ ShopAdapter.java
@@ -1,6 +1,6 @@
 package com.example.market;
 
-import android.content.Context; 
+import android.content.Context;  
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View; 
